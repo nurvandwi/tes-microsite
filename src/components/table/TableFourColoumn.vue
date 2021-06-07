f1251248058d4cb2b14a1f9e5cfd007e1e8aff4b
nurvandwi <nurvandwi20@gmail.com>
2021-06-07 10:47:46 +0700

    update table

--- src/components/table/TableFourColoumn.vue
+++ src/components/table/TableFourColoumn.vue
@@ -2,87 +2,85 @@
   <div class="mt-5">
     <b-container>
       <div id="tableNasional">
-        <div class="row text-center rows-col-3 py-3 mb-5" slot="tableAkumulasi">
-          <div
-            class="bg-grey text-white py-2 font16 font-weight-bold font-custom"
-          >
-            RINGKASAN PENJUALAN PER QUARTER
+        <div class="row rows-col-3 py-3 mb-5" slot="tableAkumulasi">
+          <div class="bg-grey">
+            <p class="text-white m-0 text-center font16 font-weight-bold">
+              RINGKASAN PENJUALAN PER QUARTER
+            </p>
             <b-row cols="4" class="my-2">
-              <b-col
-                cols="3"
-                class="align-self-center py-1 bg_gray mx-1 text-white rounded-pill text-center"
-              >
-                <p class="m-0 font12">QUARTER</p>
+              <b-col cols="3" class="py-1 bg_gray mx-1 text-white rounded-pill">
+                <p class="m-0 font12 text-center">QUARTER</p>
               </b-col>
-              <b-col
-                cols="3"
-                class="align-self-center py-1 bg_gray mx-1 text-white rounded-pill text-center"
-              >
-                <p class="m-0 font12">TARGET</p>
+              <b-col cols="3" class="py-1 bg_gray mx-1 text-white rounded-pill">
+                <p class="m-0 font12 text-center">TARGET</p>
               </b-col>
-              <b-col
-                cols="3"
-                class="align-self-center py-1 bg_gray mx-1 text-white rounded-pill text-center"
-              >
-                <p class="m-0 font12">AKTUAL</p>
+              <b-col cols="3" class="py-1 bg_gray mx-1 text-white rounded-pill">
+                <p class="m-0 font12 text-center">AKTUAL</p>
               </b-col>
               <b-col
                 cols="3"
                 style="width: calc(15%)"
-                class="align-self-center py-1 bg_gray mx-1 text-white rounded-pill text-center"
+                class="py-1 bg_gray mx-1 text-white rounded-pill"
               >
-                <p class="m-0 font12">%</p>
+                <p class="m-0 font12 text-center">%</p>
               </b-col>
             </b-row>
           </div>
-          <div class="table-bordered col-12">
+          <div class="table-bordered col-12 p-0">
             <b-row cols="4" class="my-2">
-              <b-col
-                cols="3"
-                class="align-self-center text-dark rounded-pill text-center"
-              >
-                <span class="m-0 font12">QUARTER 1</span>
+              <b-col cols="3" class="text-dark">
+                <p class="m-0 font12">QUARTER 1</p>
               </b-col>
-              <b-col
-                cols="3"
-                class="align-self-center p-0 text-dark rounded-pill text-center"
-              >
-                <span class="m-0 font12">Rp. 400.000.000</span>
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
               </b-col>
-              <b-col
-                cols="3"
-                class="align-self-center p-0 text-dark rounded-pill text-right"
-              >
-                <div class="m-0 font12 text-right"></div>
-                Rp. 400.000.000
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
               </b-col>
-              <b-col
-                cols="3"
-                class="align-self-center text-dark rounded-pill text-center"
-              >
-                <p class="m-0 font12">100,56%</p>
+              <b-col cols="3" class="text-dark text-center">
+                <p class="m-0 font11">100,56%</p>
               </b-col>
             </b-row>
             <b-row cols="4" class="my-2">
-              <b-col
-                class="align-self-center text-dark rounded-pill text-center"
-              >
-                <p class="m-0 font12">QUARTER 1</p>
+              <b-col cols="3" class="text-dark">
+                <p class="m-0 font12">QUARTER 2</p>
               </b-col>
-              <b-col
-                class="align-self-center p-0 text-dark rounded-pill text-center"
-              >
-                <span class="m-0 font12">Rp. 2.423.123.999</span>
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
               </b-col>
-              <b-col
-                class="align-self-center p-0 text-dark rounded-pill text-center"
-              >
-                <p class="m-0 font12">Rp. 423.123.999</p>
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
               </b-col>
-              <b-col
-                class="align-self-center text-dark rounded-pill text-center"
-              >
-                <p class="m-0 font12">100,56%</p>
+              <b-col cols="3" class="text-dark text-center">
+                <p class="m-0 font11">100,56%</p>
+              </b-col>
+            </b-row>
+            <b-row cols="4" class="my-2">
+              <b-col cols="3" class="text-dark">
+                <p class="m-0 font12">QUARTER 3</p>
+              </b-col>
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
+              </b-col>
+              <b-col cols="3" class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 400.000.000</p>
+              </b-col>
+              <b-col cols="3" class="text-dark text-center">
+                <p class="m-0 font11">100,56%</p>
+              </b-col>
+            </b-row>
+            <b-row cols="4" class="my-2">
+              <b-col class="text-dark">
+                <p class="m-0 font12">TOTAL</p>
+              </b-col>
+              <b-col class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 2.423.123.999</p>
+              </b-col>
+              <b-col class="p-0 text-dark">
+                <p class="m-0 font11 text_custom">Rp. 2.423.123.999</p>
+              </b-col>
+              <b-col class="text-dark text-center">
+                <p class="m-0 font11">100,56%</p>
               </b-col>
             </b-row>
           </div>
@@ -172,6 +170,10 @@ a {
   font-size: 10px;
 }
 
+.font11 {
+  font-size: 11px;
+}
+
 .font12 {
   font-size: 12px;
 }
@@ -195,9 +197,17 @@ a {
   border-top-right-radius: 18px;
 }
 
+.text_custom {
+  text-align: center;
+}
+
 @media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
   .margin-custom {
     margin-top: -2vh;
+  }
+
+  .text_custom {
+    text-align: right;
   }
 
   .font20 {
